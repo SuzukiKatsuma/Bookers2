a264a1772ee8d390a80afe8eac07a5d83c174b15
SuzukiKatsuma <c0p31049@bunkyo.ac.jp>
2021-03-04 20:56:44 +0900

    add new

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'feb2f5c12ca1bfa7901f8515ea29ef1d61e8a862f06faf93c511c4839f14bd3ad30a137d4235a04168b83337cd68e454e26f8aeb20dd81d4bd8cca772d9ba393'
